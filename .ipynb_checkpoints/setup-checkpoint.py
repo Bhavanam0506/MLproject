e9c443949bd76df5327a81ada9a9b795c6ff889c
BhavanaMadas <bhavanamadas1214@gmail.com>
2023-10-07 11:31:11 +0530

    setup_complete

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='MLproject',
+    version='0.0.1',
+    author='Bhavana',
+    author_email='bhvanamadas1214@gmmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+)
